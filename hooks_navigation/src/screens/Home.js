293fca31f410a57034cc5edbacab0c0da30c4c81
KoSangWon <jackoss@naver.com>
2020-04-10 17:30:57 +0900

    navigation interval

--- hooks_navigation/src/screens/Home.js
+++ hooks_navigation/src/screens/Home.js
@@ -1,30 +1,87 @@
-import React, {useState, useEffect} from 'react';
-import {SafeAreaView, Text, Button} from 'react-native';
+import React, {useState, useEffect, useRef} from 'react';
+import {SafeAreaView, Text, Button, Alert} from 'react-native';
 
-export default () => {
-    const [count, setCount1] = useState(0);
-    const [count2, setCount2] = useState(0);
-    const [showText, setShowText] = useState(false);
-   
-    useEffect(() => console.log('didUpdate'));
 
-    useEffect(() => {
-        if(showText){
-            setTimeout(() => setShowText(false), 2000);
-        }
-    }) 
 
-    return (
-        <SafeAreaView>
-            <Button
-                title="Add"
-                onPress={() => {
-                    setCount1(count => count+1);
-                    setShowText(true);
-                }}
-            />
-            <Text>Count: {count}</Text>
-            {showText && <Text>올라간다!</Text>}
-        </SafeAreaView>
-    )
-}
+export default ({navigation}) => {
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+  const [showText, setShowText] = useState(false);
+  const interval = useRef(null);
+  const timeout = useRef(null);
+  useEffect(() => console.log('count changed'), [count]);
+  useEffect(() => {
+    console.log('showText changed');
+    if (showText) {
+      setTimeout(() => setShowText(false), 2000);
+    }
+  }, [showText]);
+  useEffect(() => {
+    console.log('didmount');
+  }, []);
+  return (
+    <SafeAreaView>
+      <Button
+        title="Add"
+        onPress={() => {
+          setCount(c => c + 1);
+          setShowText(true);
+          // setCount(count + 1)
+        }}
+      />
+      <Text>Count: {count}</Text>
+      {showText && <Text>Count가 올라갔어요!</Text>}
+      <Button
+        title="Add2"
+        onPress={() => {
+          setCount2(c => c + 1);
+          // setCount(count + 1)
+        }}
+      />
+      <Text>Count2: {count2}</Text>
+      <Button
+        title="Trigger Interval"
+        onPress={() => {
+          interval.current = setInterval(() => {
+            console.log('Interval Called');
+          }, 1000);
+        }}
+      />
+      <Button
+        title="Stop Interval"
+        onPress={() => {
+          clearInterval(interval.current);
+          interval.current = null;
+        }}
+      />
+
+    <Button
+        title="Trigger Alrarm"
+        onPress={() => {
+          timeout.current = setTimeout(() => {
+            Alert(alert("일어나!!"));
+          }, 2000);
+        }}
+      />
+    <Button
+        title="Stop Alarm"
+        onPress={() => {
+          clearTimeout(timeout.current);
+          timeout.current = null;
+        }}
+      />
+
+
+    <Button
+        title="Go to Home2"
+        onPress={() => {
+          navigation.navigate("Home2");
+        }}
+      />    
+
+
+
+
+    </SafeAreaView>
+  );
+};
--- hooks_navigation/src/screens/Home2.js
+++ hooks_navigation/src/screens/Home2.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {SafeAreaView, Text} from 'react-native';
 
 export default () => {
+    const interval = useRef(null);
+    useEffect(() => {
+        console.log("Hi Home2");
+        interval.current = setInterval(() => {
+            console.log("Interval Called");
+        }, 1000);
+        
+        return () => {
+            console.log("Bye Home2");
+            clearInterval(interval.current);
+            interval.current = null;
+        }
+    })
+
     return (
         <SafeAreaView>
             <Text>Home2 ~~~~</Text>
