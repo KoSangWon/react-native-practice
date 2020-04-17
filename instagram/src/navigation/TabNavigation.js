d781395a5734f4edcf2a70ff68a0f90b787f62ea
KoSangWon <jackoss@naver.com>
2020-04-17 14:31:24 +0900

    react-native-vector-icons

--- instagram/src/navigation/TabNavigation.js
+++ instagram/src/navigation/TabNavigation.js
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Home from '../screens/Home';
 import Search from '../screens/Search';
 import Upload from '../screens/Upload';
 import Like from '../screens/Like';
 import Setting from '../screens/Setting';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
 
 
 const Tab = createBottomTabNavigator();
@@ -13,10 +17,29 @@ const Tab = createBottomTabNavigator();
 const MyTabs = () => {
     return (
       <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.screen name="Search" component={Search}/>
-          
+        <Tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}>
+          <Tab.Screen name="Home" 
+            component={Home} 
+            />
           <Tab.Screen name="Settings" component={Setting} />
         </Tab.Navigator>
       </NavigationContainer>
